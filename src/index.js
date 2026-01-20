1d1e145e886fea6e06b1b64ead684fbf5e427582
PrajwalJadhav4113 <prajwalj509@gmail.com>
2026-01-20 10:25:29 +0530

    db connecting

---
+++ src/index.js
@@ -0,0 +1,28 @@
+
+  
+import mongoose from "mongoose";
+import { DB_NAME} from "./constants";
+import connectDB from "./db";
+
+
+connectDB()
+// its frist approch
+/*
+(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERROR:",error);
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listning on port ${process.env.PORT}`)
+        })
+    }catch(error){
+        console.error("ERROR:",error)
+        throw err
+    }
+})()
+
+*/
